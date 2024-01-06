723c40e01627563d6eb2473b4b8e9a5f918beeef
sadid369 <sadid.jones@gmail.com>
2024-01-06 20:38:43 +0600

    46:00 finished counter app with bloc

---
+++ lib/app_state.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class AppStates {
+  int counter;
+  AppStates({
+    required this.counter,
+  });
+}
+
+class InitStates extends AppStates {
+  InitStates() : super(counter: 0);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ulearning_app/app_blocs.dart';
+import 'package:ulearning_app/app_events.dart';
+import 'package:ulearning_app/app_state.dart';
+// import 'dart:developer' as devtools show log;
+// import 'package:flutter/foundation.dart';
+
+// extension Log on Object {
+//   void log() => devtools.log(toString());
+// }
 
 void main() {
-  runApp(const MyApp());
+  runApp(BlocProvider(
+    create: (context) => AppBlocs(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -41,31 +56,48 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    log('build');
+
     return Scaffold(
-      appBar: AppBar(
-        elevation: 15,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        appBar: AppBar(
+          elevation: 15,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: BlocBuilder<AppBlocs, AppStates>(builder: (context, snapshot) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '${snapshot.counter}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            );
+          }),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                context.read<AppBlocs>().add(Increment());
+              },
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            FloatingActionButton(
+              onPressed: () {
+                context.read<AppBlocs>().add(Decrement());
+              },
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        ));
   }
 }
